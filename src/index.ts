7ee372ae7922c2b7485f7d4ed6cc4ed1ecb5e8ce
moneko97 <poi.nyaa@qq.com>
2024-01-22 16:58:38 +0800

    remove mock module

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@
 ":" //# comment; exec /usr/bin/env node --experimental-modules --experimental-json-modules "$0" "$@"
 
 import { program } from 'commander';
-import { sigintOrsigTerm } from './exit.js';
 import './version.js';
 import './eslint.js';
 import './stylelint.js';
@@ -14,7 +13,14 @@ import './git-hooks.js';
 import './changelog.js';
 
 program.option('-ig,--initgit', 'init git');
-
 program.parse(process.argv);
 
-sigintOrsigTerm();
+// 发送 SIGINT、 SIGTERM 和 SIGKILL 会导致目标进程被无条件地终止
+// 然后子进程会报告该进程已被信号终止
+// 'SIGINT' 在终端运行时，可以被所有平台支持
+// 'SIGTERM' 在 Windows 中不支持，可以给其绑定监听器
+['SIGINT', 'SIGTERM', 'SIGQUIT'].forEach((signal) =>
+  process.on(signal, () => {
+    process.exit(1);
+  })
+);
