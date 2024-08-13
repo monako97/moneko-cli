14555a1a44d1a99104ecf2a1399b09075516a16b
moneko97 <poi.nyaa@qq.com>
2024-08-14 00:52:08 +0800

    feat: @inquirer/prompts

--- src/build-app.ts
+++ src/build-app.ts
@@ -3,7 +3,7 @@ import fs from 'fs';
 import path from 'path';
 import readline from 'readline';
 import { program } from 'commander';
-import inquirer, { InputQuestion } from 'inquirer';
+import { input, checkbox } from '@inquirer/prompts';
 import shell from 'shelljs';
 import { bundleApk, bundleIpa } from './utils/bundle-app.js';
 import {
@@ -19,222 +19,203 @@ import { cwd } from './utils/config.js';
 import { loadFileSync, saveFile } from '@moneko/utils';
 
 const createApp = async () => {
-  interface InputQuestions extends InputQuestion {
-    // eslint-disable-next-line no-unused-vars
-    async: () => (err: string | null, ok?: boolean) => void;
-  }
-  inquirer
-    .prompt([
+  const bundleId = await input({
+    message: '请输入软件包名称?',
+    default: 'com.moneko.bid',
+    validate(value) {
+      const isOk = /(^com\.)([a-zA-Z_][a-zA-Z0-9_]*[.])*([a-zA-Z_][a-zA-Z0-9_]*)$/.test(value);
+
+      return !isOk || value.split('.').length !== 3 ? '软件包名不合法' : true;
+    },
+  });
+  const bundles = await checkbox({
+    message: '请选择需要打包的类型',
+    validate(val) {
+      return val.length === 0 ? '请选择打包类型' : true;
+    },
+    choices: [
       {
-        type: 'input',
-        name: 'bundleId',
-        message: '请输入软件包名称?',
-        default: 'com.moneko.bid',
-        validate: function (input: string) {
-          const done = (this as unknown as InputQuestions).async();
-          const isOk = /(^com\.)([a-zA-Z_][a-zA-Z0-9_]*[.])*([a-zA-Z_][a-zA-Z0-9_]*)$/.test(input);
+        name: '打包为apk (release)',
+        value: 'apk -release',
+      },
+      {
+        name: '打包为apk (debug)',
+        value: 'apk -debug',
+      },
+      {
+        name: '打包为ipa (release)[未签名]',
+        value: 'ios -release',
+      },
+      {
+        name: '打包为ipa (debug)[未签名]',
+        value: 'ios -debug',
+      },
+    ],
+  });
+  const domain = bundleId.split('.');
+  const bundle_id = domain.splice(-1, 1).join('');
+  const webAssetsEntry = path.join(cwd, '/dist');
 
-          if (!isOk || input.split('.').length !== 3) {
-            done('软件包名不合法');
-            return;
-          }
+  const sh = `flutter create -t app --org ${bundleId} -i swift -a kotlin ${bundle_id}`;
 
-          done(null, true);
+  const outputPath = path.join(cwd, '/' + bundle_id);
+
+  shell.exec(sh, { silent: true });
+  shell.cd(outputPath);
+
+  const webAssetsOutput = path.join(outputPath, 'assets/web/');
+
+  shell.exec('mkdir -p ' + webAssetsOutput, { silent: true });
+
+  process.stdout.write(chalk.yellow('正在注册资产...'));
+  // 拷贝 dist
+  copyFolderRecursiveSync(webAssetsEntry, webAssetsOutput);
+  // pub 依赖
+  const pubObjPath = path.join(outputPath, 'pubspec.yaml');
+  const pubObj: Record<string, Record<string, string | string[]>> = yamlToJson(
+    loadFileSync(pubObjPath) || ''
+  ) as Record<string, Record<string, string | string[]>>;
+  // 自动分析注册资产
+  const allAssetsDirs: string[] = [];
+  const getAssetsDir = (dirPath: string, baseDir = 'assets/web') => {
+    const files = fs.readdirSync(dirPath);
+
+    // 迭代器 异步变同步
+    (function iterator(i) {
+      if (i == files.length) {
+        return;
+      }
+      const stats = fs.statSync(dirPath + '/' + files[i]);
+
+      if (stats.isDirectory()) {
+        allAssetsDirs.push(baseDir + '/' + files[i] + '/');
+        getAssetsDir(dirPath + '/' + files[i], baseDir + '/' + files[i]);
+      }
+      iterator(i + 1);
+    })(0);
+  };
+
+  getAssetsDir(webAssetsEntry);
+
+  pubObj.dependencies.webview_flutter_plus = '^0.2.3';
+  pubObj.flutter.assets = ['assets/web/', ...allAssetsDirs];
+  saveFile(pubObjPath, jsonToYaml(pubObj));
+
+  readline.cursorTo(process.stdout, 0);
+  process.stdout.write(chalk.cyan('注册资产') + ': ' + chalk.green('完成\n'));
+  // 安卓
+  if (bundles.includes('apk -release') || bundles.includes('apk -debug')) {
+    // 安卓权限配置
+    const androidUsesPermissions = [
+      {
+        $: {
+          'android:name': 'android.permission.INTERNET',
         },
       },
       {
-        type: 'checkbox',
-        name: 'bundles',
-        message: '请选择需要打包的类型',
-        choices: [
-          {
-            key: 'bundles',
-            name: '打包为apk (release)',
-            value: 'apk -release',
-          },
-          {
-            key: 'bundles',
-            name: '打包为apk (debug)',
-            value: 'apk -debug',
-          },
-          {
-            key: 'bundles',
-            name: '打包为ipa (release)[未签名]',
-            value: 'ios -release',
-          },
-          {
-            key: 'bundles',
-            name: '打包为ipa (debug)[未签名]',
-            value: 'ios -debug',
-          },
-        ],
+        $: {
+          'android:name': 'android.permission.ACCESS_NETWORK_STATE',
+        },
       },
-    ])
-    .then(async ({ bundleId, bundles }: { bundleId: string; bundles: string[] }) => {
-      const domain = bundleId.split('.');
-      const bundle_id = domain.splice(-1, 1).join('');
-      const webAssetsEntry = path.join(cwd, '/dist');
+      {
+        $: {
+          'android:name': 'android.permission.WAKE_LOCK',
+        },
+      },
+    ];
 
-      const sh = `flutter create -t app --org ${bundleId} -i swift -a kotlin ${bundle_id}`;
+    process.stdout.write(chalk.yellow('正在配置安卓权限...'));
+    // 安卓 配置文件
+    const androidMainXmlPath = path.join(outputPath, 'android/app/src/main/AndroidManifest.xml');
+    const androidMainXmlJson = await xmlToJson(loadFileSync(androidMainXmlPath) || '');
 
-      const outputPath = path.join(cwd, '/' + bundle_id);
+    androidMainXmlJson.manifest.application.$['android:usesCleartextTraffic'] = 'true';
+    androidMainXmlJson.manifest['uses-permission'] = androidUsesPermissions;
 
-      shell.exec(sh, { silent: true });
-      shell.cd(outputPath);
+    const androidMainXml = jsonToXml(androidMainXmlJson);
 
-      const webAssetsOutput = path.join(outputPath, 'assets/web/');
+    saveFile(androidMainXmlPath, androidMainXml);
+    // 修改 安卓build.gradle
+    const androidBuildGradlePath = path.join(outputPath, 'android/app/build.gradle');
+    let androidBuildGradle = loadFileSync(androidBuildGradlePath) || '';
+    const minSdkVersion = 'minSdkVersion flutter.minSdkVersion';
 
-      shell.exec('mkdir -p ' + webAssetsOutput, { silent: true });
+    if (androidBuildGradle?.includes(minSdkVersion)) {
+      androidBuildGradle = androidBuildGradle.replace(minSdkVersion, 'minSdkVersion 20');
+    } else {
+      const minSdkVersionIndex = androidBuildGradle.indexOf('minSdkVersion ');
+      const len = androidBuildGradle.substring(minSdkVersionIndex, minSdkVersionIndex + 16);
 
-      process.stdout.write(chalk.yellow('正在注册资产...'));
-      // 拷贝 dist
-      copyFolderRecursiveSync(webAssetsEntry, webAssetsOutput);
-      // pub 依赖
-      const pubObjPath = path.join(outputPath, 'pubspec.yaml');
-      const pubObj: Record<string, Record<string, string | string[]>> = yamlToJson(
-        loadFileSync(pubObjPath) || ''
-      ) as Record<string, Record<string, string | string[]>>;
-      // 自动分析注册资产
-      const allAssetsDirs: string[] = [];
-      const getAssetsDir = (dirPath: string, baseDir = 'assets/web') => {
-        const files = fs.readdirSync(dirPath);
+      androidBuildGradle = androidBuildGradle.replace(len, 'minSdkVersion 20');
+    }
+    const compileSdkVersion = 'compileSdkVersion flutter.compileSdkVersion';
+    if (androidBuildGradle.includes(compileSdkVersion)) {
+      androidBuildGradle = androidBuildGradle.replace(compileSdkVersion, 'compileSdkVersion 32');
+    }
 
-        // 迭代器 异步变同步
-        (function iterator(i) {
-          if (i == files.length) {
-            return;
-          }
-          const stats = fs.statSync(dirPath + '/' + files[i]);
-
-          if (stats.isDirectory()) {
-            allAssetsDirs.push(baseDir + '/' + files[i] + '/');
-            getAssetsDir(dirPath + '/' + files[i], baseDir + '/' + files[i]);
-          }
-          iterator(i + 1);
-        })(0);
-      };
-
-      getAssetsDir(webAssetsEntry);
-
-      pubObj.dependencies.webview_flutter_plus = '^0.2.3';
-      pubObj.flutter.assets = ['assets/web/', ...allAssetsDirs];
-      saveFile(pubObjPath, jsonToYaml(pubObj));
-
-      readline.cursorTo(process.stdout, 0);
-      process.stdout.write(chalk.cyan('注册资产') + ': ' + chalk.green('完成\n'));
-      // 安卓
-      if (bundles.includes('apk -release') || bundles.includes('apk -debug')) {
-        // 安卓权限配置
-        const androidUsesPermissions = [
-          {
-            $: {
-              'android:name': 'android.permission.INTERNET',
-            },
-          },
-          {
-            $: {
-              'android:name': 'android.permission.ACCESS_NETWORK_STATE',
-            },
-          },
-          {
-            $: {
-              'android:name': 'android.permission.WAKE_LOCK',
-            },
-          },
-        ];
-
-        process.stdout.write(chalk.yellow('正在配置安卓权限...'));
-        // 安卓 配置文件
-        const androidMainXmlPath = path.join(
-          outputPath,
-          'android/app/src/main/AndroidManifest.xml'
-        );
-        const androidMainXmlJson = await xmlToJson(loadFileSync(androidMainXmlPath) || '');
-
-        androidMainXmlJson.manifest.application.$['android:usesCleartextTraffic'] = 'true';
-        androidMainXmlJson.manifest['uses-permission'] = androidUsesPermissions;
-
-        const androidMainXml = jsonToXml(androidMainXmlJson);
-
-        saveFile(androidMainXmlPath, androidMainXml);
-        // 修改 安卓build.gradle
-        const androidBuildGradlePath = path.join(outputPath, 'android/app/build.gradle');
-        let androidBuildGradle = loadFileSync(androidBuildGradlePath) || '';
-        const minSdkVersion = 'minSdkVersion flutter.minSdkVersion';
-
-        if (androidBuildGradle?.includes(minSdkVersion)) {
-          androidBuildGradle = androidBuildGradle.replace(minSdkVersion, 'minSdkVersion 20');
-        } else {
-          const minSdkVersionIndex = androidBuildGradle.indexOf('minSdkVersion ');
-          const len = androidBuildGradle.substring(minSdkVersionIndex,  minSdkVersionIndex + 16);
-  
-          androidBuildGradle = androidBuildGradle.replace(len, 'minSdkVersion 20');
-        }
-        const compileSdkVersion = 'compileSdkVersion flutter.compileSdkVersion';
-        if (androidBuildGradle.includes(compileSdkVersion)) {
-            androidBuildGradle = androidBuildGradle.replace(compileSdkVersion, 'compileSdkVersion 32');
-        }
-        
-        saveFile(androidBuildGradlePath, androidBuildGradle);
-        readline.cursorTo(process.stdout, 0);
-        process.stdout.write(chalk.cyan('安卓权限配置') + ': ' + chalk.green('完成 \n'));
-      }
-      // ios
-      if (bundles.includes('ios -release') || bundles.includes('ios -debug')) {
-        process.stdout.write(chalk.yellow('开始配置IOS权限'));
-        // ios 配置文件
-        const iosPlistPath = path.join(outputPath, 'ios/Runner/Info.plist');
-        const iosPlist = loadFileSync(iosPlistPath) || '';
-        const plistJson = Object.assign(plistToJson(iosPlist), {
-          NSAppTransportSecurity: {
-            NSAllowsArbitraryLoads: true,
-          },
-          ['io.flutter.embedded_views_preview']: true,
-        });
-
-        saveFile(iosPlistPath, jsonToPlist(plistJson));
-        readline.cursorTo(process.stdout, 0);
-        process.stdout.write(chalk.cyan('IOS权限配置') + ': ' + chalk.green('完成\n'));
-      }
-      // 主程序
-      saveFile(
-        path.join(outputPath, 'lib/main.dart'),
-        loadFileSync(path.join(__dirname, '../conf/bootstrap')) || ''
-      );
-
-      const outputBundleDir = path.join(cwd, './bundle/');
-
-      shell.exec('mkdir -p ' + outputBundleDir, { silent: true });
-      // 文件准备ok
-      setTimeout(() => {
-        bundles.forEach((item: string) => {
-          const bundleItem = item.split(' ');
-
-          if (bundleItem[0] === 'apk') {
-            bundleApk(outputPath, outputBundleDir, bundleItem[1]);
-          }
-          if (bundleItem[0] === 'ios') {
-            bundleIpa(outputPath, outputBundleDir, bundleItem[1]);
-          }
-        });
-        shell.exec(`rm -rf ${outputPath}`, { silent: true });
-
-        if (fs.existsSync(path.join(outputBundleDir, 'Runner'))) {
-          shell.exec(`rm -rf ${path.join(outputBundleDir, 'Runner')}`, {
-            silent: true,
-          });
-        }
-
-        readline.cursorTo(process.stdout, 0);
-        process.stdout.write('✨ ' + chalk.green('Done'));
-        process.exit(0);
-      }, 1000);
+    saveFile(androidBuildGradlePath, androidBuildGradle);
+    readline.cursorTo(process.stdout, 0);
+    process.stdout.write(chalk.cyan('安卓权限配置') + ': ' + chalk.green('完成 \n'));
+  }
+  // ios
+  if (bundles.includes('ios -release') || bundles.includes('ios -debug')) {
+    process.stdout.write(chalk.yellow('开始配置IOS权限'));
+    // ios 配置文件
+    const iosPlistPath = path.join(outputPath, 'ios/Runner/Info.plist');
+    const iosPlist = loadFileSync(iosPlistPath) || '';
+    const plistJson = Object.assign(plistToJson(iosPlist), {
+      NSAppTransportSecurity: {
+        NSAllowsArbitraryLoads: true,
+      },
+      ['io.flutter.embedded_views_preview']: true,
     });
+
+    saveFile(iosPlistPath, jsonToPlist(plistJson));
+    readline.cursorTo(process.stdout, 0);
+    process.stdout.write(chalk.cyan('IOS权限配置') + ': ' + chalk.green('完成\n'));
+  }
+  // 主程序
+  saveFile(
+    path.join(outputPath, 'lib/main.dart'),
+    loadFileSync(path.join(__dirname, '../conf/bootstrap')) || ''
+  );
+
+  const outputBundleDir = path.join(cwd, './bundle/');
+
+  shell.exec('mkdir -p ' + outputBundleDir, { silent: true });
+  // 文件准备ok
+  setTimeout(() => {
+    bundles.forEach((item: string) => {
+      const bundleItem = item.split(' ');
+
+      if (bundleItem[0] === 'apk') {
+        bundleApk(outputPath, outputBundleDir, bundleItem[1]);
+      }
+      if (bundleItem[0] === 'ios') {
+        bundleIpa(outputPath, outputBundleDir, bundleItem[1]);
+      }
+    });
+    shell.exec(`rm -rf ${outputPath}`, { silent: true });
+
+    if (fs.existsSync(path.join(outputBundleDir, 'Runner'))) {
+      shell.exec(`rm -rf ${path.join(outputBundleDir, 'Runner')}`, {
+        silent: true,
+      });
+    }
+
+    readline.cursorTo(process.stdout, 0);
+    process.stdout.write('✨ ' + chalk.green('Done'));
+    process.exit(0);
+  }, 1000);
 };
 
 program
   .command('buildApp')
   .description('编译将 h5 应用打包成移动客户端，使用 Flutter')
   .action(() => {
-    createApp();
+    try {
+      createApp();
+    } catch (error) {
+      //
+    }
   });
--- src/create.ts
+++ src/create.ts
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import { program } from 'commander';
-import inquirer from 'inquirer';
+import { input, checkbox, select } from '@inquirer/prompts';
 import { __dirname } from './file.js';
 import {
   cliAlias,
@@ -23,17 +23,25 @@ import { fetchTemplate } from './utils/template.js';
 import { loadFileSync, saveFile } from '@moneko/utils';
 
 type Framework = 'react' | 'vue' | 'solid';
+type AppType = 'admin-panel' | 'web-portal' | 'micro-frontend' | 'mobile-app' | 'component-library';
 const genFiles = (options: {
   name: string;
-  type: string;
+  type: AppType;
   framework: Framework;
   author: string;
   tools: string[];
   destination?: string;
 }) => {
   const { name, type, framework, tools } = options;
-  const isLibrary = type === 'library';
-  const templateName = `templet-${type}-${framework}`;
+  const isLibrary = type === 'component-library';
+  const appTypeName = {
+    'admin-panel': 'backstage',
+    'web-portal': 'site',
+    'micro-frontend': 'micro',
+    'mobile-app': 'mobile',
+    'component-library': 'library',
+  }[type];
+  const templateName = `templet-${appTypeName}-${framework}`;
   const _destination = options.destination;
   // 项目指定生成目录，如果命令中没有有配置目录，则在当前命令运行的目录下生成以项目名称为名字的新目录
   const destination = _destination ? path.resolve(_destination) : path.resolve(cwd, name);
@@ -123,8 +131,8 @@ const genFiles = (options: {
     );
     pkgJson.name = options.name;
     pkgJson.author = options.author;
-    pkgJson.scripts.start = `${cliAlias} start ${type} ${framework}`;
-    pkgJson.scripts.build = `${cliAlias} build ${type} ${framework}`;
+    pkgJson.scripts.start = `${cliAlias} start ${appTypeName} ${framework}`;
+    pkgJson.scripts.build = `${cliAlias} build ${appTypeName} ${framework}`;
     pkgJson.version = '1.0.0';
     pkgJson.main = 'index.js';
     pkgJson.files = undefined;
@@ -240,114 +248,114 @@ const genFiles = (options: {
   });
 };
 
-const handleCreate = (
-  params: Record<string, string | boolean | number>,
+async function handleCreate(
+  params: { name: string; destination?: string },
   options: Record<string, string | boolean | number>
-) => {
-  inquirer
-    // 用户交互
-    .prompt([
+) {
+  const author = await input({
+    message: '请输入开发者名称?',
+    default: cliName,
+  });
+  const type = await select<AppType>({
+    message: '您想创建一个什么类型的项目呢?',
+    choices: [
       {
-        type: 'input',
-        name: 'author',
-        message: '请输入开发者名称?',
-        default: cliName,
+        name: '后台管理平台',
+        value: 'admin-panel',
+        description:
+          '适用于创建功能齐全的后台管理界面，无论是企业级应用还是个人网站，均可实现高效的数据管理和操作流程',
       },
       {
-        type: 'list',
-        name: 'type',
-        message: '您想创建一个什么类型的项目呢?',
-        choices: [
-          {
-            key: 'type',
-            name: '后台管理(Backstage)',
-            value: 'backstage',
-          },
-          {
-            key: 'type',
-            name: '门户网站(site)',
-            value: 'site',
-          },
-          {
-            key: 'type',
-            name: '微前端(Micro Frontends)',
-            value: 'micro',
-          },
-          {
-            key: 'type',
-            name: '移动端 H5应用(Mobile)',
-            value: 'mobile',
-          },
-          {
-            key: 'type',
-            name: '组件库(Component Library)',
-            value: 'library',
-          },
-        ],
+        name: '信息门户',
+        value: 'web-portal',
+        description:
+          '适合构建内容丰富的门户网站，无论是企业官网还是个人网站，都能提供统一的风格和多样化的信息展示功能',
       },
       {
-        type: 'list',
-        name: 'framework',
-        message: '框架',
-        choices: [
-          {
-            key: 'framework',
-            name: 'React',
-            value: 'react',
-          },
-          {
-            key: 'framework',
-            name: 'Solid.js',
-            value: 'solid',
-          },
-        ],
+        name: '微前端应用',
+        value: 'micro-frontend',
+        description:
+          '微前端是一种架构模式，通过将大型应用拆分为多个独立的小型应用模块，提升开发灵活性与效率，适合复杂的前端项目',
       },
       {
-        type: 'checkbox',
-        name: 'tools',
-        message: '请选择需要开启的开发辅助功能',
-        choices: [
-          {
-            key: 'tools',
-            name: 'css代码规范',
-            value: stylelintPackageName,
-          },
-          {
-            key: 'tools',
-            name: 'javascript、typescript代码规范',
-            value: eslintPackageName,
-          },
-          {
-            key: 'tools',
-            name: '根据 commit 信息生成 Changelog',
-            value: 'changelog',
-          },
-          {
-            key: 'tools',
-            name: 'Git commit 规范',
-            value: 'commitlint',
-          },
-          {
-            key: 'tools',
-            name: 'Git hooks(使用husky)',
-            value: huskyPackageName,
-          },
-        ],
-        default: [stylelintPackageName, eslintPackageName],
+        name: '移动端应用',
+        value: 'mobile-app',
+        description: '适用于开发跨平台移动应用，使用 Flutter 打包成 App，提供优质的用户体验',
       },
-    ])
-    .then((answers) => {
-      // 根据回答以及选项，参数来生成项目文件
-      genFiles({ ...answers, ...params, ...options });
-    })
-    .catch((error) => {
-      throw new Error(error);
-    });
-};
+      {
+        name: '组件库',
+        value: 'component-library',
+        description:
+          '组件库是一组可复用的功能模块集合，经过深度打磨，能够帮助开发者快速构建高质量的应用，从而提升开发效率并降低成本',
+      },
+    ],
+    loop: false,
+  });
+  const framework = await select<Framework>({
+    message: '您希望使用什么 JavaScript 框架',
+    choices: [
+      {
+        name: 'React',
+        value: 'react',
+        description:
+          'React 是一个用于构建 Web 和原生交互界面的库，广泛应用于开发灵活且高效的用户界面',
+      },
+      {
+        name: 'Solid',
+        value: 'solid',
+        description:
+          'Solid 是一个现代化的 JavaScript 框架，专注于构建响应迅速且高性能的用户界面（UI），以其简单且可预测的开发体验，适合各个层次的开发者',
+      },
+      {
+        name: 'Vue',
+        value: 'vue',
+        description:
+          'Vue 是一个用于构建用户界面的渐进式框架。与其他大型框架不同，Vue 设计为自下而上逐层应用。核心库专注于视图层，易于上手，并且能够与第三方库或现有项目轻松集成。此外，结合现代工具链和各种支持库，Vue 也能驱动复杂的单页应用',
+        disabled: '敬请期待!',
+      },
+    ],
+    loop: false,
+  });
+  const tools = await checkbox({
+    message: '请选择需要开启的开发辅助功能',
+    choices: [
+      {
+        name: 'CSS 代码风格检查',
+        value: stylelintPackageName,
+        checked: true,
+      },
+      {
+        name: 'JavaScript / TypeScript 代码质量检查',
+        value: eslintPackageName,
+        checked: true,
+      },
+      {
+        name: '自动生成变更日志',
+        value: 'changelog',
+      },
+      {
+        name: 'Git 提交信息规范',
+        value: 'commitlint',
+      },
+      {
+        name: 'Git Hooks (Husky)',
+        value: huskyPackageName,
+      },
+    ],
+    loop: false,
+  });
+
+  // 根据回答以及选项，参数来生成项目文件
+  genFiles({ author, type, framework, tools, ...params, ...options });
+}
 
 program
   .command('create <name> [destination]')
   .description('创建一个新项目')
   .action((name, destination) => {
-    handleCreate({ name, destination }, program.opts());
+    try {
+      handleCreate({ name, destination }, program.opts());
+    } catch (error) {
+      //
+    }
   });
